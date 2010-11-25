0f2a0bf21e3b815767c2b01faa1ef8380e8311ba
alexey petrov <alexey.petrov.nnov@gmail.com>
2010-11-25 21:55:07 +0200

    to handle test scripts that produce multiple log files ("s3/g3_amazon_different_s3location", for example)

--- common.sh
+++ common.sh
@@ -84,11 +84,12 @@ process_error()
 #------------------------------------------------------------------------------------------
 run_script()
 {
-    a_test_log_name=./log.`basename ${0}`
-    if [ -f ${a_test_log_name} ]; then
+    a_test_log_name=log.`basename ${0}`
+    a_test_log_names=`ls | grep ${a_test_log_name}`
+    if [ "${a_test_log_names}x" != "x" ]; then
 	a_script_name=${0}; 
-	a_user_log_name=`dirname ${0}`/log.`basename ${0}`
-	echo "rm ${a_user_log_name} # before run '${a_script_name}'"
+	a_user_log_name=`dirname ${0}`/${a_test_log_name}
+	echo "rm ${a_user_log_name}* # before run '${a_script_name}'"
 	exit 0
     else
 	a_function=${1}; ${a_function}
