22f9625b36f3afe37da6a5fce562ba38c08ebb34
Andrey Simurzin <asimurzin@gmail.com>
2010-12-10 15:00:27 +0300

    rm iteration over parameters ( remove extraneous "_" from "studies_" )

--- s3/common_s3.sh
+++ s3/common_s3.sh
@@ -61,9 +61,7 @@ create_list_case_files()
 #-----------------------------------------------------------------------------------------
 get_study()
 {
-  an_old_api_number=${1}
-
-  echo `sed '$!d' ${TEST_CLOUDFLU_FILE_STUDIES_STARTS}_${an_old_api_number}`
+  echo `sed '$!d' ${TEST_CLOUDFLU_FILE_STUDIES_STARTS}`
 }
 
 
@@ -97,11 +95,11 @@ create_study_name()
 calc_path_to_case_dir()
 {
   a_curdir=`python -c "import os; print os.path.basename( os.path.abspath(os.path.curdir) )"`
-  if [ "$a_curdir" == "old_api" ]; then
-     echo "${TEST_CLOUDFLU_CASE_DIR_NAME}" 
-  fi
+
   if [ "$a_curdir" == "s3" ]; then
      echo "${TEST_CLOUDFLU_CASE_DIR_NAME}" 
+  else
+       echo "../${TEST_CLOUDFLU_CASE_DIR_NAME}" 
   fi
 }
 
@@ -109,20 +107,15 @@ calc_path_to_case_dir()
 #-----------------------------------------------------------------------------------------
 create_study()
 {
-  an_old_api_number=${1}
-  a_path_to_api=`calc_path_to_api $an_old_api_number`
   a_list_filenames=`create_list_filenames_in_casedir`
   a_list_files=`create_list_case_files ${a_list_filenames}`
   a_study_name=`create_study_name`
 
-  a_testing_script="${a_path_to_api}cloudflu-upload-start --study-name=${a_study_name} ${a_list_files}"
-  if [ "${a_path_to_api}x" == "x" ] ; then
-      a_testing_script="cloudflu-study-book --study-name=${a_study_name} | cloudflu-upload-start ${a_list_files}"
-  fi
+  a_testing_script="cloudflu-study-book --study-name=${a_study_name} | cloudflu-upload-start ${a_list_files}"
 
-  process_script "${a_testing_script} | ${a_path_to_api}cloudflu-upload-resume" ${an_old_api_number}
+  process_script "${a_testing_script} | ${a_path_to_api}cloudflu-upload-resume"
 
-  echo ${a_study_name} >> ${TEST_CLOUDFLU_FILE_STUDIES_STARTS}_${an_old_api_number}
+  echo ${a_study_name} >> ${TEST_CLOUDFLU_FILE_STUDIES_STARTS}
 
   export a_result=${a_study_name}
 }
@@ -134,7 +127,7 @@ prepare_new_testing_data()
   an_old_api_number=${1}
   
   if [ "x${an_old_api_number}" == "x" ]; then
-     create_study ${an_old_api_number} 
+     create_study
   else
      create_old_api_study ${an_old_api_number} 
   fi
@@ -146,7 +139,7 @@ prepare_testing_data()
 { 
   an_old_api_number=${1}
 
-  if [ ! -f ${TEST_CLOUDFLU_FILE_STUDIES_STARTS}_${an_old_api_number} ]; then
+  if [ ! -f ${TEST_CLOUDFLU_FILE_STUDIES_STARTS} ]; then
      prepare_new_testing_data ${an_old_api_number}
   else
      a_study_name=`get_study ${an_old_api_number}`
@@ -155,7 +148,7 @@ prepare_testing_data()
      fi
   fi
 
-  export __PROCESS_SCRIPT_RESULT__=`get_study ${an_old_api_number}`
+  export __PROCESS_SCRIPT_RESULT__=`get_study`
 }
      
 
