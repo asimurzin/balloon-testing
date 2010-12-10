063a9490f22cf9e1b230ebe03645ae8cbcee9369
Andrey Simurzin <asimurzin@gmail.com>
2010-12-10 12:24:46 +0300

    to rm iteration over parameter

--- s3/old_api/ls_pipe_rm_files.sh
+++ s3/old_api/ls_pipe_rm_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#----------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------
 ## Copyright 2010 Alexey Petrov
 ##
 ## Licensed under the Apache License, Version 2.0 (the "License");
@@ -32,31 +32,31 @@ test_hook()
 
   a_script_name=`basename $0`
   an_old_api_number=${1}
-  prepare_testing_data ${an_old_api_number} && a_study_name=`get_result`
+  prepare_new_testing_data ${an_old_api_number} && a_study_name=`get_result`
 
-  process_script "cloudflu-ls ${a_study_name}" ${an_old_api_number} && a_result=`get_result`
+  process_script "cloudflu-ls ${a_study_name} | xargs cloudflu-rm --study-name=${a_study_name}" ${an_old_api_number}
 
+  a_results=`cloudflu-ls ${a_study_name} 2>&1`
   an_allright="True"
-  a_list_filenames=`create_list_filenames_in_casedir`
-  for a_filename in ${a_list_filenames}; do
-      count_entrances=`echo ${a_result} | grep -c ${a_filename}`
-      if [ ${count_entrances} -eq 0 ]; then
+  a_testing_filenames=`create_list_filenames_in_casedir`
+  for a_filename in ${a_testing_filenames} ; do
+      count_entrances=`echo ${a_results} | grep -c ${a_filename}`
+      if [ ${count_entrances} -ne 0 ]; then
           an_allright="False"
-          a_not_exists_file=${a_filename}
+          a_not_deleted_file=${a_filename}
           break
       fi
-  done 
+  done
 
   if [ "${an_allright}" = "False" ]; then
-      process_error "There is no '${a_not_exists_file}' file in the S3" ${an_old_api_number}
+      process_error "The '${a_not_deleted_file}' file have not been removed from the S3" ${an_old_api_number}
+  else
+      cloudflu-study-rm ${a_study_name} > /dev/null 2>&1
+      unregister_study ${TEST_CLOUDFLU_FILE_STUDIES_STARTS}_${an_old_api_number} ${a_study_name}
   fi
 
   echo '----------------------------------- OK -----------------------------------------'
 }
-
-
-#-----------------------------------------------------------------------------------------
-run_old_api_script test_hook
 
 
 #-----------------------------------------------------------------------------------------
